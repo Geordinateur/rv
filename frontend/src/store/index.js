f8a5a48b1408bf7e2fb84f49cfa7c0178989f100
Geordinateur <emuricardo@gmail.com>
2022-03-25 06:23:11 +0100

    Continue refactory real-vue

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex'
+import auth from './auth.module'
 
 export default createStore({
   state: {
@@ -11,5 +12,6 @@ export default createStore({
   actions: {
   },
   modules: {
+    auth
   }
 })
--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -1,45 +1,9 @@
-<script>
-import axios from 'axios'
-import AlertItem from '../components/AlertItem'
-
-axios.defaults.baseURL = 'http://192.168.1.33:3000/api'
-
-export default {
-  components: {
-		AlertItem
-  },
-  data()  {
-    return {
-			alert: {},
-      form: {}
-    }
-  },
-  methods: {
-    send: function (event) {
-      event.preventDefault()
-      console.log(this.form)
-      axios
-        .post('/user/register', this.form)
-        .then((response) => {
-					this.alert.message = response.data.message
-					this.alert.status = 'alert-success'
-					this.alert.show = true
-        })
-        .catch((error) => {
-					this.alert.message = error.response.data.message
-					this.alert.show = true
-					this.alert.status = 'alert-danger'
-        })
-    },
-	}
-}
-</script>
 <template>
 <AlertItem :message="alert.message" :status="alert.status" :show="alert.show" />
-<form class="form" method="post" @submit="send">
+<form class="form" method="post" @submit.prevent="onSubmit">
 <div class="form-group">
-<label for="name">Pseudonyme</label>
-<input type="text" class="form-control" id="name" v-model="form.name" placeholder="Entrez un pseudo">
+<label for="name">Username</label>
+<input type="text" class="form-control" id="name" v-model="username" placeholder="Entrez un pseudo">
 </div>
 <div class="form-group">
 <label for="email">Email address</label>
@@ -48,12 +12,43 @@ export default {
 </div>
 <div class="form-group">
 <label for="password">Mot de passe</label>
-<input type="password" class="form-control" id="password" v-model="form.password" placeholder="Password">
+<input type="password" class="form-control" id="password" v-model="password" placeholder="Password">
 </div>
 <div class="form-group">
 <label for="passwordVerify">Confirmez votre mot de passe</label>
-<input type="password" class="form-control" id="passwordVerify" v-model="form.passwordVerify" placeholder="Password">
+<input type="password" class="form-control" id="passwordVerify" v-model="passwordVerify" placeholder="Password">
 </div>
 <button type="submit" class="btn btn-primary mt-3">Enregister</button>
 </form>
 </template>
+
+<script>
+import { mapState } from "vuex";
+import { REGISTER } from "@/store/actions.type";
+export default {
+  name: "RegisterView",
+  data() {
+    return {
+      username: "",
+      email: "",
+      password: ""
+    };
+  },
+  computed: {
+    ...mapState({
+      errors: state => state.auth.errors
+    })
+  },
+  methods: {
+    onSubmit() {
+      this.$store
+        .dispatch(REGISTER, {
+          email: this.email,
+          password: this.password,
+          username: this.username
+        })
+        .then(() => this.$router.push({ name: "home" }));
+    }
+  }
+};
+</script>
